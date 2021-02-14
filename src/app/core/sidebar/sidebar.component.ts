f6f44d6d03f2a1d129869d45b551262847a08380
Diego Alves Santana <diegoalsantana@gmail.com>
2021-02-14 18:10:08 -0300

    Adicao de label no sidebar

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  changeText: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
